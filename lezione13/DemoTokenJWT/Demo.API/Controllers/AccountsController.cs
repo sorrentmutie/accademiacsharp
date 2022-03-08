5731eb781fb248074de66f428688c0859f28adc3
salvatore <salvatore.sorrentino@live.com>
2022-03-08 13:35:23 +0100

    lezione 13

--- lezione13/DemoTokenJWT/Demo.API/Controllers/AccountsController.cs
+++ lezione13/DemoTokenJWT/Demo.API/Controllers/AccountsController.cs
@@ -3,6 +3,10 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Demo.API.Controllers
 {
@@ -39,9 +43,60 @@ namespace Demo.API.Controllers
                 return StatusCode(StatusCodes.Status201Created, new { result.Succeeded });
             } else
             {
-                return StatusCode(500);
+                var errors = "Registrazione fallita";
+                foreach (var error in result.Errors)
+                {
+                    errors += $"Codice di errore: {error.Code} Descrizione: {error.Description}";
+                    errors += Environment.NewLine;
+                }
+                return StatusCode(StatusCodes.Status500InternalServerError, errors);
             }
         }
+
+        [NonAction]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        private async Task<string> GeneraJSONWebToken(IdentityUser identityUser)
+        {
+            var symmetricKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(configuration["Jwt:SecurityKey"]));
+            var credentials = new SigningCredentials(symmetricKey, SecurityAlgorithms.HmacSha256);
+
+            IList<string> roleNames = await userManager.GetRolesAsync(identityUser);
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, identityUser.Id),
+                new Claim(ClaimTypes.Email, identityUser.Email),
+                new Claim(ClaimTypes.Name, identityUser.Email),
+                new Claim(JwtRegisteredClaimNames.Sub, identityUser.Email ),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            }.Union(roleNames.Select(role => new Claim(ClaimTypes.Role, role)));
+
+            JwtSecurityToken jwtSecurityToken = new JwtSecurityToken
+                (configuration["Jwt:Issuer"], configuration["Jwt:Audience"],
+                claims, DateTime.UtcNow, DateTime.UtcNow.AddMinutes(10),
+                credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+        }
+
+        [HttpPost]
+        [Route("signin")]
+        public async Task<IActionResult> SignIn([FromBody] RegisterRequest user)
+        {
+            var signInResult = await signInManager.PasswordSignInAsync(
+                 user.Email, user.Password, false, false);
+            if(signInResult.Succeeded == true)
+            {
+                IdentityUser identityUser = await userManager.FindByEmailAsync(user.Email);
+                var jwtToken = await GeneraJSONWebToken(identityUser);
+                return Ok(jwtToken);
+            } else
+            {
+                return Unauthorized(user);
+            }
+        }
+
 
     }
 }
---
+++ lezione13/DemoTokenJWT/Demo.BlazorWASM/Security/AppAuthenticationStateProvider.cs
@@ -0,0 +1,74 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Demo.BlazorWASM.Security
+{
+    public class AppAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService localStorageService;
+        private readonly JwtSecurityTokenHandler jwtSecurityTokenHandler = new();
+
+        public AppAuthenticationStateProvider(ILocalStorageService localStorageService)
+        {
+            this.localStorageService = localStorageService;
+        }
+
+        public async Task SignIn()
+        {
+            string jwtToken = await localStorageService.GetItemAsync<string>("bearerToken");
+            JwtSecurityToken jwtSecurityToken = jwtSecurityTokenHandler.ReadJwtToken(jwtToken);
+            IList<Claim> claims = jwtSecurityToken.Claims.ToList();
+            claims.Add(new Claim(ClaimTypes.Name, jwtSecurityToken.Subject));
+            var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "jwt"));
+            Task<AuthenticationState> authentication =
+                 Task.FromResult(new AuthenticationState(user));
+            NotifyAuthenticationStateChanged(authentication);
+        }
+
+        public void SignOut()
+        {
+            ClaimsPrincipal utenteUscito = new ClaimsPrincipal(new ClaimsIdentity());
+            Task<AuthenticationState> authentication =
+                 Task.FromResult(new AuthenticationState(utenteUscito));
+            NotifyAuthenticationStateChanged(authentication);
+        }
+
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            try
+            {
+                string jwtToken = await localStorageService.GetItemAsStringAsync("bearerToken");
+
+                if(string.IsNullOrWhiteSpace(jwtToken))
+                {
+                    return new AuthenticationState(
+                        new ClaimsPrincipal(new ClaimsIdentity { }));
+                }
+
+                JwtSecurityToken jwtSecurityToken = jwtSecurityTokenHandler.ReadJwtToken(jwtToken);
+                DateTime expires = jwtSecurityToken.ValidTo;
+                if(expires < DateTime.UtcNow)
+                {
+                    await localStorageService.RemoveItemAsync("bearerToken");
+                    return new AuthenticationState(
+                        new ClaimsPrincipal(new ClaimsIdentity { }));
+                }
+
+                IList<Claim> claims = jwtSecurityToken.Claims.ToList();
+                claims.Add(new Claim(ClaimTypes.Name, jwtSecurityToken.Subject));
+                var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "jwt"));
+                return new AuthenticationState(user);
+
+            }
+            catch (Exception)
+            {
+
+               return new AuthenticationState(
+                   new ClaimsPrincipal(new ClaimsIdentity { }));
+            }
+        }
+    }
+}
